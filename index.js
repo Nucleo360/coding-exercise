6484d31b31b0e5ef82a1fdaa51a173ceadc67a92
developer-somosocialmedia <158046338+developer-somosocialmedia@users.noreply.github.com>
2026-02-03 13:55:49 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * EJERCICIO 1: Palíndromo
+ * Implementa isPalindrome(text) -> boolean
+ */
+
+/**
+ * EJERCICIO 2: Contar vocales
+ * Implementa countVowels(text) -> number
+ */
+
+/**
+ * EJERCICIO 3: Aplanar arrays anidados (deep flatten)
+ * Implementa flattenDeep(arr) -> any[]
+ */
+
+function isPalindrome(text) {
+  // TODO: implementa aquí
+}
+
+function countVowels(text) {
+  // TODO: implementa aquí
+}
+
+function flattenDeep(arr) {
+  // TODO: implementa aquí
+}
+
+module.exports = {
+  isPalindrome,
+  countVowels,
+  flattenDeep,
+};
